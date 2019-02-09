a736dc79446124066c50c39f7c9c8589e3151d26
Shanik Makalande <sh4nik@gmail.com>
2019-02-09 17:42:54 +0530

    Index.js moved to src; Build changed to dist;

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Edible from './Edible';
+import Food from './Food';
+import Poison from './Poison';
+import Brain from './Brain';
+import Simulation from './Simulation';
+import Agent from './Agent';
+
+(function (window) {
+  window.BloopLabs = { Edible, Food, Poison, Brain, Simulation, Agent };
+})(window);
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
 const path = require('path');
 const config = {
-    entry: [path.resolve(__dirname, 'index.js')],
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'blooplabs-engine.js'
-    },
-    node: { fs: 'empty' },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js']
-    },
-    devServer: {
-        port: 3000,
-        contentBase: __dirname + '/build',
-        inline: true
-    },
-    stats: {
-        colors: true
-    }
+  entry: [path.resolve(__dirname, 'src/index.js')],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'blooplabs.js'
+  },
+  node: { fs: 'empty' },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  devServer: {
+    port: 3000,
+    contentBase: __dirname + '/dist',
+    inline: true
+  },
+  stats: {
+    colors: true
+  }
 };
 
 module.exports = config;
