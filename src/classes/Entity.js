6c6d2f6204e4229369aac8cbde6e1cbb3cffa76b
sh4nik <sh4nik@gmail.com>
2019-02-21 12:32:05 +0530

    Size added to entity

--- src/classes/Entity.js
+++ src/classes/Entity.js
@@ -1,14 +1,16 @@
 class Entity {
-  constructor ({
+  constructor({
     isActive = true,
     position,
     group = 'default',
-    sortRank = 999
+    sortRank = 999,
+    size = 20
   }) {
     this.isActive = isActive;
     this.group = group;
     this.position = position;
     this.sortRank = sortRank;
+    this.size = size;
   }
 }
 
